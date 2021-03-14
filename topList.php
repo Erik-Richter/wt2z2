dadabbdfbf8785a8aade0dcf6f0a4b45589607eb
erik-ctrl <richtererik20@gmail.com>
2021-03-14 20:50:19 +0100

    first table finished

--- topList.php
+++ topList.php
@@ -1,1 +1,35 @@
-<?php
+<!doctype html>
+
+<html lang="sk">
+<head>
+  <meta charset="utf-8">
+
+  <title>Top 10</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/style.css">
+
+</head>
+
+<body>
+<h1><a id="pageHeader" href="index.php"> <img src="img/13765.png" alt="O" height="51" width="51">lympijské hry</a></h1>
+
+    <main>
+        <table class="table table-dark table-hover">
+            <thead>
+                <tr>
+                    <th>Miesto v rebríčku</th>
+                    <th>Meno</th>
+                    <th>Priezvisko</th>
+                    <th>Počet zlatých medailí</th>
+                    <th>Úpravy</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php include ("topListL.php"); ?>
+                <td><img src="img/gold-trophy.png" </td>
+            </tbody>
+        </table>
+    </main>
+</body>
+</html>
---
+++ topListL.php
@@ -0,0 +1,42 @@
+<?php
+
+include_once("../config.php");
+$dbname = "";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=zadanie2", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //echo "<h1>Olympijské Hry</h1>";
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage() . "<br>";
+}
+
+$sql = "SELECT person.id, person.name,person.surname, COUNT(placing.placing) AS golds 
+                            FROM placing,person where placing.person_id=person.id 
+                                                  and placing.placing = 1 
+                            group by person.id, person.name, person.surname order by golds desc limit 10";
+$stm = $conn->query($sql);
+$top10 = $stm->fetchAll(PDO::FETCH_ASSOC);
+
+
+$iterator = 0;
+foreach ($top10 as $person){
+    $iterator++;
+    echo '<tr>';
+    echo '<td>' .$iterator .'</td>';
+    echo '<td><a class="inverseLink" href="detail.php?id=' .$person["id"]. '">' .$person["name"] .'</a></td>';
+    echo '<td><a class="inverseLink" href="detail.php?id=' .$person["id"]. '">' .$person["surname"] .'</a></td>';
+    echo '<td>' ;for ($i=0; $i<$person['golds']; $i++){
+        echo '<img src="img/gold-trophy.png">';
+    }
+    echo '</td>';
+    echo '<td>' .'<a href="edit.php?id=' . $person["id"]. '">'.'Upraviť</a>'.'/'.'<a class="warn" href="delete.php?id=' . $person["id"]. '">'.'Vymazať</a>' .'</td>';
+    echo '</tr>';
+}
+
+function printTrophy($value){
+    for ($i=0; $i<$value; $i++){
+        echo '<img src="img/gold-trophy.png">';
+    }
+}
